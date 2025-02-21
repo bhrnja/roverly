a3530da5f5fe31e96759185ad690a1f81f4ee01d
Bea Hrnja <bhrnja.mail@gmail.com>
2025-02-20 22:12:31 -0500

    Created a simple nav menu with basic styling. The navbar interactivity for both desktop and mobile versions has been built. Basic styling applied until more permanent solutions can be implemented.

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.querySelector(".mobile-nav");
+
+function showMobileMenu() {
+    mobileMenu.style.display = "flex";
+}
+
+function closeMobileMenu() {
+    mobileMenu.style.display = "none";
+}
